d0ffd22b30c3fe523d210692851563bb79b67bab
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-03 09:58:20 -0700

    Add build script

---
+++ build-bullseye-amd64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+rm -rf docker-debian-artifacts
+git clone --depth 1 --branch dist-amd64 --filter=blob:none --sparse git@github.com:jgowdy/docker-debian-artifacts.git
+cd docker-debian-artifacts
+git sparse-checkout init --cone
+git sparse-checkout set bullseye
+cd bullseye
+docker build . -t jgowdy-bullseye:latest
+cd slim
+docker build . -t jgowdy-slim-bullseye:latest
+
