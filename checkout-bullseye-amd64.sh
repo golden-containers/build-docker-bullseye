6d37f0fe78ff74b621aad989f2e4388d2a58d79b
Jeremiah Gowdy <jgowdy@godaddy.com>
2021-11-05 19:59:23 -0700

    Update checkout-bullseye-amd64.sh

--- checkout-bullseye-amd64.sh
+++ checkout-bullseye-amd64.sh
@@ -9,6 +9,3 @@ git sparse-checkout init --cone
 git sparse-checkout set bullseye
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//ghcr.io\/golden-containers\/bullseye/" bullseye/backports/Dockerfile
-
-pwd
-find .
