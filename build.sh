a545c6a812b4e68bd8d9ba214613eceba5f2082d
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 12:47:06 -0800

    Try -a

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ docker build bullseye/backports --tag ghcr.io/golden-containers/debian:bullseye-
 
 # Push
 
-docker push debian
+docker push -a
 # ghcr.io/golden-containers/bullseye:latest
 
 #docker push ghcr.io/golden-containers/bullseye-slim:latest
