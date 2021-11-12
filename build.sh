6679bb8e21ea419870cf9d6843399fb2f33b76dd
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 11:14:26 -0800

    Add gsed for macOS

--- build.sh
+++ build.sh
@@ -14,7 +14,10 @@ git sparse-checkout set bullseye
 
 # Transform
 
-sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye/; t" -e "1,// s//ghcr.io\/golden-containers\/debian\:bullseye/" bullseye/backports/Dockerfile
+# This sed syntax is GNU sed specific
+[ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
+
+${GNU_SED} -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye/; t" -e "1,// s//ghcr.io\/golden-containers\/debian\:bullseye/" bullseye/backports/Dockerfile
 
 # Build
 
